97be467eeb8d6a396ab2935515280813a8371712
Shashi Kumar <shashikumar85796@gmail.com>
2026-06-06 19:14:38 +0530

    fix: remove .vercel/output from git, add to .gitignore

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DeLIynLN.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Gargi singh Rajput/Downloads/Shashi Work/Chandrashekhar Profile Site/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-D8lkA0dj.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-D8lkA0dj.js" } }] }, "/": { filePath: "C:/Users/Gargi singh Rajput/Downloads/Shashi Work/Chandrashekhar Profile Site/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BERuPy8J.js"] } } });
-export {
-  tsrStartManifest
-};
